af67476a967296c38d70e564c6c256cd8b056ea4
Levi Durfee <levi.durfee@gmail.com>
2017-05-27 17:07:53 -0400

    move call to header

--- mersenne.h
+++ mersenne.h
@@ -54,4 +54,36 @@ void get_binary(int n, char *str) {
     str = lol_reverse(str);
 }
 
+void mersenne() {
+	double m;
+	m = 929;
+	int divider = 47;
+	char bits[6];
+	get_binary(m, bits);
+	printf("%s\n", bits);
+
+	int remainder = 1;
+	int i = 0;
+	int square;
+	int result;
+	do {
+	    square = pow(remainder, 2);
+	    //printf("Remainder: %i Squared: %i\n", remainder, square);
+	    if(bits[i] == '\0') {
+	        break;
+	    }
+	    if(bits[i] == '1') {
+	        printf("mul\n");
+	        result = square * 2;
+	    } else {
+	        printf("no mul\n");
+	        result = square;
+	    }
+	    //printf("Result: %i Mod: %i\n", result, d);
+	    remainder = fmod(result, divider);
+	    i++;
+	    printf("%i\n", remainder);
+	} while(remainder != 1);
+}
+
 #endif
--- test.c
+++ test.c
@@ -3,33 +3,5 @@
 #include "mersenne.h"
 
 int main() {
-    double m;
-    m = 929;
-    int divider = 47;
-    char bits[6];
-    get_binary(m, bits);
-    printf("%s\n", bits);
-
-    int remainder = 1;
-    int i = 0;
-    int square;
-    int result;
-    do {
-        square = pow(remainder, 2);
-        //printf("Remainder: %i Squared: %i\n", remainder, square);
-        if(bits[i] == '\0') {
-            break;
-        }
-        if(bits[i] == '1') {
-            printf("mul\n");
-            result = square * 2;
-        } else {
-            printf("no mul\n");
-            result = square;
-        }
-        //printf("Result: %i Mod: %i\n", result, d);
-        remainder = fmod(result, divider);
-        i++;
-        printf("%i\n", remainder);
-    } while(remainder != 1);
+    mersenne();
 }
